693098cea27ea90815fac479bd76a2aa94205cc8
Mike <earlyprogrammer@yahoo.com>
2014-12-01 14:56:25 -0700

    quick server + static

--- server/main.js
+++ server/main.js
@@ -1,13 +1,22 @@
 var express = require("express");
 
 var app = express();
-var router = express.Router();
+var staticRouter = express.Router();
+var apiRouter = express.Router();
 
-router.get("/", function(req, res){
+staticRouter.use(express.static("../public"));
+
+apiRouter.get("/public/:email", function(req, res){
 	res.status = 200;
-	res.json({ "asdf": "qwer"});
+	res.json({ "key": "something weird looking"});
 });
 
-app.use("/", router);
+apiRouter.get("/private/:email", function(req, res){
+	res.status = 200;
+	res.json({ "key": "haha, like we'd tell you the private key!"});
+});
+
+app.use("/api", apiRouter);
+app.use("/", staticRouter);
 
 app.listen(8080);
