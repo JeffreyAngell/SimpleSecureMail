252b30f87d459a2bf2ecdca05f6e345577161263
Mike <earlyprogrammer@yahoo.com>
2014-12-01 16:11:50 -0700

    first connections

--- server/main.js
+++ server/main.js
@@ -4,16 +4,20 @@ var app = express();
 var staticRouter = express.Router();
 var apiRouter = express.Router();
 
+var keys = require("./custom/KeyAccess/keyaccess");
+
 staticRouter.use(express.static("../public"));
 
 apiRouter.get("/public/:email", function(req, res){
-	res.status = 200;
-	res.json({ "key": "something weird looking"});
+	keys.getPublic(req.params.email, function(key){
+		res.status(200).json({ "key": key});
+	});
 });
 
 apiRouter.get("/private/:email", function(req, res){
-	res.status = 200;
-	res.json({ "key": "haha, like we'd tell you the private key!"});
+	keys.getPrivate(req.params.email, function(key){
+		res.status(200).json({ "key": key});
+	});
 });
 
 app.use("/api", apiRouter);
